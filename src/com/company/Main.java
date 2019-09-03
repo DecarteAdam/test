98c6780109de4f256e48bbb3860ba51d3d34fb72
Moussa <moussa@MacBook-Pro-de-Moussa.local>
2019-09-03 18:35:01 +0200

    My first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int tab[] = {2, 13, 4, 5, 8, 9, 12, 6, 18, 47};
+        for (int bite = 0; bite < 10; bite++) {
+            System.out.println("a l'indice bite: " + bite + " le tableau renvoi " + tab[bite]);
+        }
+
+
+
+
+      int resultat = calcule('a', 'b');
+
+      System.out.println("\nresultat:"+ resultat);
+
+
+
+
+
+    }
+
+    public static int calcule(char x, char y){
+
+        int resultat = x;
+
+        return resultat;
+
+    }
+
+}
